92c0832bf684788533f3b30ae312c73be097d200
Ayelet Danieli <ayeletd@gmail.com>
2021-03-30 11:13:37 +0300

    19_1

--- hello/src/Components/E19_1_Simple_Fetch/App.jsx
+++ hello/src/Components/E19_1_Simple_Fetch/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import MyMovie from "./MyMovie";
 
-const lorem =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam excepturi obcaecati, ea dicta perferendis, provident tenetur natus est aliquam sequi unde, numquam sed laboriosam impedit dolores exercitationem facilis voluptatem nam.";
 const App = () => {
   return <MyMovie />;
 };
